91474dd92c36133adc47d539b0b4e377bf78a39e
pyvan23 <ivanmiguelalvarez@gmail.com>
2022-08-25 20:22:14 -0300

    auth store,redux

---
+++ README.md
@@ -0,0 +1,8 @@
+# Calendar App
+
+
+## development steps
+
+1- Rename file .env.template by .env 
+
+2- make changes  variables enviroment
--- src/store/store.js
+++ src/store/store.js
@@ -1,10 +1,12 @@
 import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
+import { authSlice } from "./auth/authSlice";
 import { calendarSlice } from "./calendar/calendarSlice";
 import { uiSlice } from "./ui/uiSlice";
 
 
 export const store = configureStore({
     reducer:{
+        auth:authSlice.reducer,
         calendar:calendarSlice.reducer,
         ui:uiSlice.reducer
     },middleware:(getDefaultMiddleware)=>getDefaultMiddleware({
